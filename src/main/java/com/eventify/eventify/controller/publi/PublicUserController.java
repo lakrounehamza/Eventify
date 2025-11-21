cf3c323ceb07d928ff33cae1dac5747c56da2764
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-21 14:27:19 +0100

    Add PublicUserController with user registration endpoint

--- src/main/java/com/eventify/eventify/controller/publi/PublicUserController.java
+++ src/main/java/com/eventify/eventify/controller/publi/PublicUserController.java
@@ -1,4 +1,26 @@
 package com.eventify.eventify.controller.publi;
 
+import com.eventify.eventify.dto.request.UserRegisterRequest;
+import com.eventify.eventify.dto.response.UserResponse;
+import com.eventify.eventify.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/public/users")
+@RequiredArgsConstructor
 public class PublicUserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRegisterRequest request) {
+        UserResponse response = userService.save(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
