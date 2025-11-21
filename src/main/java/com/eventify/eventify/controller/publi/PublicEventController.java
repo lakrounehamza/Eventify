7edea537e15e9f7e723310f989e65167bbaeb1d7
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-21 14:26:15 +0100

    Add PublicEventController to expose paginated public events

--- src/main/java/com/eventify/eventify/controller/publi/PublicEventController.java
+++ src/main/java/com/eventify/eventify/controller/publi/PublicEventController.java
@@ -1,4 +1,22 @@
 package com.eventify.eventify.controller.publi;
 
+
+import com.eventify.eventify.dto.response.EventResponse;
+import com.eventify.eventify.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/public/events")
+@RequiredArgsConstructor
 public class PublicEventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public Page<EventResponse> getAllEvents(Pageable pageable) {
+        return eventService.getAll(pageable);
+    }
 }
