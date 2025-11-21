039f03a7761f8370dd01f6970c492502e8d2aeaa
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-21 12:17:48 +0100

    feat(service)  : ajouter  interface  pour  user service

--- src/main/java/com/eventify/eventify/service/UserService.java
+++ src/main/java/com/eventify/eventify/service/UserService.java
@@ -1,4 +1,15 @@
 package com.eventify.eventify.service;
 
-public class UserService {
+import com.eventify.eventify.dto.request.RegistrationCreateRequest;
+import com.eventify.eventify.dto.response.RegistrationResponse;
+import org.springframework.data.domain.Page;
+
+import java.util.UUID;
+
+public interface UserService {
+    public RegistrationResponse save(RegistrationCreateRequest registrationRequest);
+    public RegistrationResponse getEventById(UUID id);
+    public RegistrationResponse update(UUID id ,RegistrationResponse registrationResponseRequest);
+    public String  delete(UUID id);
+    public Page<RegistrationResponse> getAll();
 }
