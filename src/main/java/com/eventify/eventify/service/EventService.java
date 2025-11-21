51598aa3e6454a74cfea0d2f8d71a458fcf29e21
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-21 12:18:42 +0100

    feat(service)  : ajouter  interface  pour  event service

--- src/main/java/com/eventify/eventify/service/EventService.java
+++ src/main/java/com/eventify/eventify/service/EventService.java
@@ -1,4 +1,16 @@
 package com.eventify.eventify.service;
 
-public class EventService {
+import com.eventify.eventify.dto.request.EventCreateRequest;
+import com.eventify.eventify.dto.request.EventUpdateRequest;
+import com.eventify.eventify.dto.response.EventResponse;
+import org.springframework.data.domain.Page;
+
+import java.util.UUID;
+
+public interface EventService {
+    public EventResponse save(EventCreateRequest eventRequest);
+    public EventResponse getEventById(UUID id);
+    public EventResponse update(UUID id , EventUpdateRequest eventRequest);
+    public String  delete(UUID id);
+    public Page<EventResponse> getAll();
 }
