96e3edd028cd821c4669baf43d62b8cf72a34224
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-21 17:22:29 +0100

    feat(config) :  ajoute  configuratin  de  authoriration

--- src/main/java/com/eventify/eventify/config/SecurityConfig.java
+++ src/main/java/com/eventify/eventify/config/SecurityConfig.java
@@ -1,5 +1,28 @@
 package com.eventify.eventify.config;
 
+import com.eventify.eventify.entity.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole(Role.ROLE_ADMIN.toString())
+                        .requestMatchers("/organizer/**").hasRole(Role.ROLE_ORGANIZER.toString())
+                        .requestMatchers("/user/**").hasRole(Role.ROLE_USER.toString())
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
 }
