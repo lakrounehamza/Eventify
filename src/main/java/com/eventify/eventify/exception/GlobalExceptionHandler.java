772a9335caabde7739c430ba5245234d74b1dc16
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-22 10:45:44 +0100

    feat(exception) :  ajouter  handler  de methode de exception pour validation les iputs

--- src/main/java/com/eventify/eventify/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eventify/eventify/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.eventify.eventify.exception;
 
+import com.eventify.eventify.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        ErrorResponse response = new ErrorResponse();
+        String errors = e.getBindingResult().getAllErrors()
+                .stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        response.setError(errors);
+        response.setStatus(400);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
 }
