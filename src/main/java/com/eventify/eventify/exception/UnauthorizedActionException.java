067bddae758e8c2c246b7e0fad52c22743916898
lakrounehamza <lakrounehamza10@gmail.com>
2025-11-22 10:32:22 +0100

    feat(exception)  :  ajouter les   exception

--- src/main/java/com/eventify/eventify/exception/UnauthorizedActionException.java
+++ src/main/java/com/eventify/eventify/exception/UnauthorizedActionException.java
@@ -1,4 +1,7 @@
 package com.eventify.eventify.exception;
 
-public class UnauthorizedActionException {
+public class UnauthorizedActionException  extends  RuntimeException{
+    public UnauthorizedActionException(String message){
+        super(message);
+    }
 }
